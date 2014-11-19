be7ab06466b4093d645cf0555573de21f50340ed
Forrest Hopkins <fingerheroes@gmail.com>
2014-11-19 13:54:29 -0700

    Version 1.0

--- PredefinedBattles/src/PredefinedBattlesMain.java
+++ PredefinedBattles/src/PredefinedBattlesMain.java
@@ -3,28 +3,27 @@ public class PredefinedBattlesMain {
 
 	public static void main(String[] args) {
 		Scanner keyInput = new Scanner(System.in);
-		String version = "0.8";
-		int team1Members = 0;
-		int team2Members = 0;
-		int team1Strength = 1;
-		int team2Strength = 1;
+		String version = "1.0";
 		int team1Power;
 		int team2Power;
-		int team1Array[] = new int[2];
-		int team2Array[] = new int[2];
-		Team team = new Team();
 		Team team1;
+		Team team2;
 		
 		System.out.println("Welcome to Predefined Battles, version " + version);
 		System.out.println("Let's start with Team 1.");
 		team1 = new Team("Team 1");
 		team1Power = team1.getPower();
 		
+		/** Old non-parametric way of doing this
 		System.out.println("Great! Let's move on to Team 2.");
 		team2Array = team.teamQuantify("Team 2");
 		team2Members = team2Array[0];
 		team2Strength = team2Array[1];
-		team2Power = team.getPower(team2Members, team2Strength);
+		team2Power = team.getPower(team2Members, team2Strength);**/
+		
+		System.out.println("Great! Let's move on to Team 2.");
+		team2 = new Team("Team 2");
+		team2Power = team2.getPower();
 		
 		if (team1Power > team2Power) {
 			System.out.println("Team 1 won!");
